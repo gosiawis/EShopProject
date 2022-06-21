4387f8bec4ab877f90994bb87a20ca76d89d80d5
gosiawis <malgosia.wisniewska@hotmail.com>
2022-06-21 13:17:39 -0700

    Deleted obsolete table and column

--- DatabaseEShop/Seed.PostDeployment1.sql
+++ DatabaseEShop/Seed.PostDeployment1.sql
@@ -8,11 +8,11 @@ Post-Deployment Script Template
  Example:      :setvar TableName MyTable							
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
-
+*/
 
 INSERT INTO brands VALUES ('Chanel', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Dior', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Lancome', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Fenty Beauty', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Rare Beauty', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Huda Beauty', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME());
 INSERT INTO categories VALUES ('Lips',GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Eyes', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Face', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Brows', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Body', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Hair', GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME());
-INSERT INTO products VALUES ('Dior Addict Lip Glow', 2, 1, 35.00, 45, NULL, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('BACKSTAGE Eyeshadow Palette', 2, 2, 49.00, 23, NULL, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Killawatt Freestyle Highlighter', 4, 3, 38.00, 11, NULL, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Stunna Lip Paint Longwear Fluid Lip Color', 4, 1, 26.00, 0, NULL, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Fly Baby Mini Eye Primer and Liner Set', 4, 2, 24.00, 0, NULL, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME());
+INSERT INTO products VALUES ('Dior Addict Lip Glow', 2, 1, 35.00, 45, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('BACKSTAGE Eyeshadow Palette', 2, 2, 49.00, 23, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Killawatt Freestyle Highlighter', 4, 3, 38.00, 11, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Stunna Lip Paint Longwear Fluid Lip Color', 4, 1, 26.00, 0, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME()), ('Fly Baby Mini Eye Primer and Liner Set', 4, 2, 24.00, 0, GetDate(), SUSER_NAME(), GetDate(), SUSER_NAME());
 INSERT INTO voivodeships VALUES ('Lower Silesia'), ('Kuyavia-Pomerania'), ('Lodzkie'), ('Lublin');
 INSERT INTO order_statuses VALUES ('New'), ('In progress'), ('Completed'), ('Closed'), ('Canceled');
 INSERT INTO payment_statuses VALUES ('In progress'), ('Received'), ('Failed');
@@ -21,4 +21,3 @@ INSERT INTO clients VALUES ('Anna', 'Kowalska', 'annak@gmail.com', 1, 'Towny', '
 INSERT INTO orders VALUES (1, 3, 2, 1, GetDate(), GetDate(), 0.00);
 INSERT INTO order_items VALUES (1, 2, 1), (1, 4, 1);
 
-*/
--- DatabaseEShop/products.sql
+++ DatabaseEShop/products.sql
@@ -8,7 +8,6 @@
     CONSTRAINT products_category_id_fk FOREIGN KEY (category_id) REFERENCES categories ([id]),
     [price] FLOAT NOT NULL, 
     [quantity] INT NOT NULL DEFAULT 0, 
-    [picture] VARBINARY(MAX) NULL DEFAULT NULL, 
     [created_date] DATETIME NOT NULL DEFAULT getdate(), 
     [createtd_by] NVARCHAR(MAX) NOT NULL DEFAULT SUSER_NAME(), 
     [last_modified_date] DATETIME NOT NULL DEFAULT getdate(), 
